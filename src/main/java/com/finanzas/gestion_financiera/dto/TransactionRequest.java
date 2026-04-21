785945df63a939d0571fea459b005c8dcae5f8f2
leniaso <alejandro.naranjoc@udea.edu.co>
2026-04-20 19:14:45 -0500

    agregacion de mensajes ante errores

--- src/main/java/com/finanzas/gestion_financiera/dto/TransactionRequest.java
+++ src/main/java/com/finanzas/gestion_financiera/dto/TransactionRequest.java
@@ -9,16 +9,18 @@ import java.time.LocalDate;
 public class TransactionRequest {
 
     @NotBlank(message = "El tipo es obligatorio")
-    @Pattern(regexp = "INGRESO|GASTO", message = "El tipo debe ser INGRESO o GASTO")
+    @Pattern(
+            regexp = "INGRESO|GASTO",
+            message = "El tipo debe ser INGRESO o GASTO"
+    )
     private String tipo;
 
     @NotNull(message = "El monto es obligatorio")
-    @DecimalMin(value = "0.01", message = "El monto debe ser mayor a 0")
+    @DecimalMin(value = "0.01", message = "Debes ingresar un monto válido")
     private BigDecimal monto;
-
-    @NotNull(message = "La fecha es obligatoria")
-    private LocalDate fecha;
 
     @NotNull(message = "La categoría es obligatoria")
     private Long categoriaId;
+
+    // fecha eliminada — se asigna automáticamente en el service
 }
